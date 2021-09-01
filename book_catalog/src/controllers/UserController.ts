88c23450ef7b678d14cdac7fbfca821169ce0a99
ElieneMontenegro <lila.amaral@hotmail.com>
2021-09-01 16:10:52 -0300

    create first controller

--- book_catalog/src/controllers/UserController.ts
+++ book_catalog/src/controllers/UserController.ts
@@ -1,48 +1,47 @@
 import { Request, Response } from "express";
 import { getRepository } from "typeorm";
 import { User } from "../entities/User";
-
-
+import { UserInterface } from "../types/User";
 
 //definir tipagens
 
 //GET '/users'
 export const getAllUsers = async (request: Request, response: Response) => {
-    const users = await getRepository(User).find();
-    return response.json(users)
+  const users = await getRepository(User).find();
+  response.json(users);
 };
 
 //GET '/users/:id'
 export const getUser = async (request: Request, response: Response) => {
-    const user = await getRepository(User).findOne(request.params.id);
-    return response.json(user)
+  const user = await getRepository(User).findOne(request.params.id);
+  return response.json(user);
 };
 
 //POST '/users'
 export const createUser = async (request: Request, response: Response) => {
-    const user = await getRepository(User).save(request.body);
-    return response.json(user)
+  const user = await getRepository(User).save(request.body);
+  return response.json(user);
 };
 
 //UPDATE '/users/:id'
 export const updateUser = async (request: Request, response: Response) => {
-    const update = await getRepository(User).update(request.params.id, request.body);
-    if(update.affected){
-        const user = await getRepository(User).findOne(request.params.id);
-        return response.json(user)
-    }
-    throw new Error('não foi possível atualizar o usuário, tente novamente.')
-    
+  const update = await getRepository(User).update(
+    request.params.id,
+    request.body
+  );
+  if (update.affected) {
+    const user = await getRepository(User).findOne(request.params.id);
+    return response.json(user);
+  }
+  throw new Error("não foi possível atualizar o usuário, tente novamente.");
 };
-
 
 //DELETE '/users/:id'
 export const deleteUser = async (request: Request, response: Response) => {
-    const deletion = await getRepository(User).delete(request.params.id);
-    if(deletion.affected){
-        const user = await getRepository(User).findOne(request.params.id);
-        return response.json(user)
-    }
-    throw new Error('não foi possível deletar o usuário, tente novamente.')
-    
+  const user = await getRepository(User).findOne(request.params.id);
+  const deletion = await getRepository(User).delete(request.params.id);
+  if (deletion.affected) {
+    return response.json({ message: "user deletado", user });
+  }
+  throw new Error("não foi possível deletar o usuário, tente novamente.");
 };
--- book_catalog/src/routes.ts
+++ book_catalog/src/routes.ts
@@ -1,17 +1,22 @@
-import {Router, Request, Response} from 'express'
+import { Router, Request, Response } from "express";
 
 const routes = Router();
-import {getAllUsers, getUser, createUser, updateUser, deleteUser} from './controllers/UserController'
+import {
+  getAllUsers,
+  getUser,
+  createUser,
+  updateUser,
+  deleteUser,
+} from "./controllers/UserController";
 
-routes.get('/', (request: Request, response: Response) => {
-    return response.json({message: 'hello world!'})
-})
+routes.get("/", (request: Request, response: Response) => {
+  return response.json({ message: "hello world!" });
+});
 
-routes.get('/users', getAllUsers)
-routes.get('/users/:id',getUser)
-routes.post('/users', createUser)
-routes.put('/users/:id', updateUser)
-routes.delete('/users/:id', deleteUser)
-
+routes.get("/users", getAllUsers);
+routes.get("/users/:id", getUser);
+routes.post("/users", createUser);
+routes.put("/users/:id", updateUser);
+routes.delete("/users/:id", deleteUser);
 
 export default routes;
